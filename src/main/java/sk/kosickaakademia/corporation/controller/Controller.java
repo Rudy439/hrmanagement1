59edff2de4a6754f34b413e5ee184095fa8ffd1a
Rudy439 <rudolf.mato@kosickaakademia.sk>
2021-03-14 23:56:13 +0100

    domaca

--- src/main/java/sk/kosickaakademia/corporation/controller/Controller.java
+++ src/main/java/sk/kosickaakademia/corporation/controller/Controller.java
@@ -5,10 +5,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.kosickaakademia.corporation.database.Database;
 import sk.kosickaakademia.corporation.entity.User;
 import sk.kosickaakademia.corporation.enumerator.Gender;
@@ -66,5 +63,13 @@ public class Controller {
     public ResponseEntity<String> getAllUsers(){
         List<User> list = new Database().getAllUsers();
         String json = new Util.getJson(list)*/
+
+    @GetMapping(path = "/user/age")
+    public ResponseEntity<String> getUsersAge(@RequestParam(value ="from") int from,
+        @RequestParam(value = "to") int to){
+        Util util = new Util();
+        String response = util.getJson(new Database().getUsersByAge(from,to));
+        return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON).body(null);
     }
+
 }
